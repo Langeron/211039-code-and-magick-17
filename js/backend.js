12b1f35e833008edd0a4c5462de33d9c7540e539
@dgoncharenko <dgoncharenko@crispwp.com>
2019-07-25 21:50:54 +0300

    Учебный проект: pimp my mage

--- js/backend.js
+++ js/backend.js
@@ -1,24 +1,51 @@
 'use strict';
 
 (function () {
-  var backend = function (url, method, onSuccess, onError, data) {
-    var status = {
-      200: 200
-    };
+  var Status = {
+    SUCCESS: 200
+  };
 
+  var Method = {
+    GET: 'GET',
+    POST: 'POST'
+  };
+
+  var Url = {
+    UPLOAD: 'https://js.dump.academy/code-and-magick',
+    LOAD: 'https://js.dump.academy/code-and-magick/data'
+  };
+
+  var load = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === status[200]) {
-        onSuccess(xhr.response);
+      if (xhr.status === Status.SUCCESS) {
+        onLoad(xhr.response);
       } else {
-        onError('Ошибка' + xhr.status);
+        onError('Ошибка ' + xhr.status);
       }
     });
-    xhr.open(method, url);
+    xhr.open(Method.GET, Url.LOAD);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === Status.SUCCESS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Ошибка ' + xhr.status);
+      }
+    });
+    xhr.open(Method.POST, Url.UPLOAD);
     xhr.send(data);
   };
 
-  window.backend = backend;
+  window.backend = {
+    load: load,
+    save: save
+  };
 })();
